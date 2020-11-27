a5a48ee81a913777e9399b3edc0b1f22e2f91000
VAIROC <123872636+VAIROC@users.noreply.github.com>
2020-11-27 16:16:23 +0000

    Update some links

--- cashaddr/base32.go
+++ cashaddr/base32.go
@@ -8,4 +8,12 @@ func newBase32Encoder(alphabet string) *base32Encoder {
 	return &base32Encoder{alphabet: alphabet}
 }
 
-func (e *base32Encoder) numToCode() 
+func (e *base32Encoder) numToCode() map[byte]rune {
+	result := make(map[byte]rune)
+	for i, r := range e.alphabet {
+		result[byte(i)] = r
+	}
+	return result
+}
+
+func (e *base32Encoder) co
