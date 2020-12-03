2debe4e9189b2bb5bfcfaad73a1dd643db43030a
VAIROC <123872636+VAIROC@users.noreply.github.com>
2020-12-03 18:58:55 +0000

    Fix crash on exit

--- cashaddr/errors.go
+++ cashaddr/errors.go
@@ -1,2 +1,9 @@
 package cashaddr
 
+import "errors"
+
+// List of errors
+var (
+	ErrInvalidCashAddr   = errors.New("[cashaddr] invalid cash addr")
+	ErrInvalidLegacyAddr = errors.New("[cashaddr] invalid legacy addr")
+	
