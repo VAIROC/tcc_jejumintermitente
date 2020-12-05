0356c37f7030032b071f4070455ccdec437866b2
VAIROC <123872636+VAIROC@users.noreply.github.com>
2020-12-05 12:18:50 +0000

    locales

--- example/example.go
+++ example/example.go
@@ -4,4 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"
+	"github.com/LanfordCai/ava/validator"
+)
+
+func validateBitcoinAddr() {
+	v := &validator.Bitcoin{}
+	addr := "19JeUHUvw23fwKeK1zZD4moKyxj1xn4Kxi"
+	result := v.Vali
