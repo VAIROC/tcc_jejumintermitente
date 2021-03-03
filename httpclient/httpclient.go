fc765349c5ccd21b2eb6697e0146153312f25d91
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-03 11:48:22 +0000

    some documentation

---
+++ httpclient/httpclient.go
@@ -0,0 +1,45 @@
+
+package httpclient
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+// DefaultTimeout ...
+const DefaultTimeout = 10 * time.Second
+
+// Get ...
+func Get(url string) (*http.Response, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultTimeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return http.DefaultClient.Do(req)
+}
+
+// Post ...
+func Post(url string, reqbody interface{}) (*http.Response, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultTimeout)
+	defer cancel()
+
+	body, err := json.Marshal(reqbody)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return nil, err
+	}
+
+	return http.DefaultClient.Do(req)
+}
