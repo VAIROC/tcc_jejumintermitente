255d01d82ca4d4049df2d92026f8e1556775bd23
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-08 09:10:25 +0000

    Add errors

--- validator/aeternity_test.go
+++ validator/aeternity_test.go
@@ -12,4 +12,5 @@ func TestAeternityValidateAddress(t *testing.T) {
 
 	var validCases = map[string]*Result{
 		"ak_2QkttUgEyPixKzqXkJ4LX7ugbRjwCDWPBT4p4M2r8brjxUxUYd": {Success, true, Normal, ""},
-		"ak_KHfXhF2J6VBt3sUgFygdbpEkWi6AKB
+		"ak_KHfXhF2J6VBt3sUgFygdbpEkWi6AKBkr9jNKUCHbpwwagzHUs":  {Success, true, Normal, ""},
+		"ak_zvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJVR":  {Success, true, Normal, ""},
