7033d0ada237d7eec40fa19c4b9e007182abeed4
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-09 06:20:54 +0000

    Add name container

--- validator/aeternity_test.go
+++ validator/aeternity_test.go
@@ -14,3 +14,8 @@ func TestAeternityValidateAddress(t *testing.T) {
 		"ak_2QkttUgEyPixKzqXkJ4LX7ugbRjwCDWPBT4p4M2r8brjxUxUYd": {Success, true, Normal, ""},
 		"ak_KHfXhF2J6VBt3sUgFygdbpEkWi6AKBkr9jNKUCHbpwwagzHUs":  {Success, true, Normal, ""},
 		"ak_zvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJVR":  {Success, true, Normal, ""},
+	}
+
+	for addr, result := range validCases {
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mainnet), result), addr)
+		assert.True(t,
