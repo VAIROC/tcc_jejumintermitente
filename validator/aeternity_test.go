8bb0c37719898a17ed7038fd355b2af27efc334e
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-13 15:38:45 +0000

    version tag

--- validator/aeternity_test.go
+++ validator/aeternity_test.go
@@ -26,4 +26,5 @@ func TestAeternityValidateAddress(t *testing.T) {
 		"akzvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJVR":    {Success, false, Unknown, ""},
 		"ak_zvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJV":    {Success, false, Unknown, ""},
 		"aK_zvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJVR":   {Success, false, Unknown, ""},
-		"
+		"th_2SYg4CBCZpCBTnYncSzE25sRUMN4uFDDKc11pMD7u73J6xaP1g ": {Success, false, Unknown, ""},
+		"bc1q3l9k4lm5z4mtsl6smmj9qxy03e65x3maz4p9xv":             {Suc
