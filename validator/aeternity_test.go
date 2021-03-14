72c0b31ed8d4fd4cc51735b14fd473fe907f6475
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-14 09:41:54 +0000

    oops.

--- validator/aeternity_test.go
+++ validator/aeternity_test.go
@@ -27,4 +27,10 @@ func TestAeternityValidateAddress(t *testing.T) {
 		"ak_zvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJV":    {Success, false, Unknown, ""},
 		"aK_zvU8YQLagjcfng7Tg8yCdiZ1rpiWNp1PBn3vtUs44utSvbJVR":   {Success, false, Unknown, ""},
 		"th_2SYg4CBCZpCBTnYncSzE25sRUMN4uFDDKc11pMD7u73J6xaP1g ": {Success, false, Unknown, ""},
-		"bc1q3l9k4lm5z4mtsl6smmj9qxy03e65x3maz4p9xv":             {Suc
+		"bc1q3l9k4lm5z4mtsl6smmj9qxy03e65x3maz4p9xv":             {Success, false, Unknown, ""},
+		"abcde": {Success, false, Unknown, ""},
+		"":      {Success, false, Unknown, ""},
+	}
+
+	for addr, result := range invalidCases {
+		assert.True(t,
