d90c75dcdb54b98f13e1662d98e25d84924f674b
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-15 12:24:38 +0000

    Remove duplicate entry.

--- validator/aeternity_test.go
+++ validator/aeternity_test.go
@@ -34,4 +34,6 @@ func TestAeternityValidateAddress(t *testing.T) {
 
 	for addr, result := range invalidCases {
 		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mainnet), result), addr)
-		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Testnet), re
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Testnet), result), addr)
+	}
+}
---
+++ validator/arweave.go
@@ -0,0 +1,23 @@
+
+package validator
+
+import "encoding/base64"
+
+// Arweave ...
+type Arweave struct{}
+
+var _ Validator = (*Arweave)(nil)
+
+// ValidateAddress returns validate result of arweave address
+func (v *Arweave) ValidateAddress(addr string, network NetworkType) *Result {
+	if len(addr) != 43 {
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	data, err := base64.RawURLEncoding.DecodeString(addr)
+	if err != nil || len(data) != 32 {
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	return &Result{Success, true, Normal, ""}
+}
