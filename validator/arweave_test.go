43ce3bdaa6f20f6470a84b3d59f3a6cb0972ba37
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-17 06:56:10 +0000

    Update weather.lua

--- validator/arweave_test.go
+++ validator/arweave_test.go
@@ -11,4 +11,7 @@ func TestArweaveValidateAddress(t *testing.T) {
 	validator := &Arweave{}
 
 	var validCases = map[string]*Result{
-		"kEGgWKgTtojj-TGdbIUPPHmntIWQEs1IPegkCH3SyZ8": {Success, 
+		"kEGgWKgTtojj-TGdbIUPPHmntIWQEs1IPegkCH3SyZ8": {Success, true, Normal, ""},
+		"181f-LjI1ooRZhj3wLK2bCjOpgUmfOo_BplG4mCGuxU": {Success, true, Normal, ""},
+		"Tk1NuG7Jxr9Ecgva5tWOJya2QGDOoS6hMZP0paB129c": {Success, true, Normal, ""},
+		
