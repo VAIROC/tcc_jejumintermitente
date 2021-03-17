20d2bae818d26b5bbeb4aa80279c5edc080b07ba
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-17 06:36:14 +0000

    works

--- validator/arweave_test.go
+++ validator/arweave_test.go
@@ -7,4 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestArweaveVali
+func TestArweaveValidateAddress(t *testing.T) {
+	validator := &Arweave{}
+
+	var validCases = map[string]*Result{
+		"kEGgWKgTtojj-TGdbIUPPHmntIWQEs1IPegkCH3SyZ8": {Success, 
