389dcb83c1e4c33de0fe484097f97bd9c4ed97fd
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-19 15:21:16 +0000

    Krazy fixes

--- validator/arweave_test.go
+++ validator/arweave_test.go
@@ -14,4 +14,5 @@ func TestArweaveValidateAddress(t *testing.T) {
 		"kEGgWKgTtojj-TGdbIUPPHmntIWQEs1IPegkCH3SyZ8": {Success, true, Normal, ""},
 		"181f-LjI1ooRZhj3wLK2bCjOpgUmfOo_BplG4mCGuxU": {Success, true, Normal, ""},
 		"Tk1NuG7Jxr9Ecgva5tWOJya2QGDOoS6hMZP0paB129c": {Success, true, Normal, ""},
-		
+		"XepqBRwnq8SNfple3WHh0VWo06P8KQ0hVlNOjOgrJ5w": {Success, true, Normal, ""},
+		"tO710xPXwCGwTPGKtOkEq3PMbWqXs9jOGiL8TCpDuw0": {Success, true, Normal, "
