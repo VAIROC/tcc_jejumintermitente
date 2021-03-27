caef2b6c37099b893a1e4550c19bf8000196c266
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-03-27 17:21:52 +0000

    Update Developer Getting Started Guide.md

--- validator/bitcoin_test.go
+++ validator/bitcoin_test.go
@@ -14,3 +14,5 @@ func TestBitcoinValidateAddress(t *testing.T) {
 		"1HX2swQNH9ezE8RagPPAaSBTcK3in9xWYF":                             {Success, true, P2PKH, ""},
 		"1NQhfGtWRwU6zg5G58TfQibHyJEuo6ZYXw":                             {Success, true, P2PKH, ""},
 		"1NT5SNNaoAXzvxRUvYGxiif93q7o9u4854":                             {Success, true, P2PKH, ""},
+		"3NJHZpnnk3bFxqVHVS2vUomUBznju6W8D9":                             {Success, true, P2SH, ""},
+		"3DtsukMi6SYqWLvE1hh5rJnHePvD77Rsga":                             {Succe
