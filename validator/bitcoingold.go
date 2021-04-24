4b3aee5f9bc1e8e0297af211b0df375acb9651ac
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-04-24 19:02:08 +0000

    for

--- validator/bitcoingold.go
+++ validator/bitcoingold.go
@@ -6,4 +6,7 @@ type BitcoinGold struct{}
 var _ BitcoinLike = (*BitcoinGold)(nil)
 
 // ValidateAddress returns validate result of bitcoingold address
-func (v *BitcoinGold) ValidateAddress(addr string, 
+func (v *BitcoinGold) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
+		return &Result{Success, true, addrType, ""}
+	
