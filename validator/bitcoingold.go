3d650914a225f91322824a94c3c90a4fdcc1ba5f
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-04-25 05:30:51 +0000

    hah

--- validator/bitcoingold.go
+++ validator/bitcoingold.go
@@ -9,4 +9,10 @@ var _ BitcoinLike = (*BitcoinGold)(nil)
 func (v *BitcoinGold) ValidateAddress(addr string, network NetworkType) *Result {
 	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
 		return &Result{Success, true, addrType, ""}
-	
+	}
+
+	return &Result{Success, false, Unknown, ""}
+}
+
+// AddressVersion returns bitcoingold address version according to the address type and
+// ne
