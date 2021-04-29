0ce39730d67216c59ee856bd5b3c8002d439a8e1
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-04-29 06:48:27 +0000

    Renaming variable

--- validator/bitcoinsv.go
+++ validator/bitcoinsv.go
@@ -6,4 +6,5 @@ type BitcoinSV struct{}
 var _ BitcoinLike = (*BitcoinSV)(nil)
 
 // ValidateAddress returns validate result of bitcoin address
-func 
+func (v *BitcoinSV) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := NormalAddrType(v, addr, network); addrType != 
