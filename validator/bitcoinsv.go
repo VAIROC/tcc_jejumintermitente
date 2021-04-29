53e7b326d6ebab9e919e4aad8ceea2e2c0b9aa6c
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-04-29 17:05:24 +0000

    ALSA: usb - Creative USB X-Fi Pro SB1095 volume knob support

--- validator/bitcoinsv.go
+++ validator/bitcoinsv.go
@@ -7,4 +7,11 @@ var _ BitcoinLike = (*BitcoinSV)(nil)
 
 // ValidateAddress returns validate result of bitcoin address
 func (v *BitcoinSV) ValidateAddress(addr string, network NetworkType) *Result {
-	if addrType := NormalAddrType(v, addr, network); addrType != 
+	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
+		return &Result{Success, true, addrType, ""}
+	}
+
+	return &Result{Success, false, Unknown, ""}
+}
+
+// AddressVersion returns bitc
