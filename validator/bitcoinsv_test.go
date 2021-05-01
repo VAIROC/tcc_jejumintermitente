ccec4ee6e563f96507b5c791b653cbc883cd0db4
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-05-01 18:33:25 +0000

    new semester

--- validator/bitcoinsv_test.go
+++ validator/bitcoinsv_test.go
@@ -1,3 +1,13 @@
 package validator
 
 import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBitcoinSVValidateAddress(t *testing.T) {
+	validator := &BitcoinSV{}
+
+	var mainnetC
