1a976622530112799315e14e3616e2d103e295a7
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-05-03 21:36:33 +0000

    Updated Occitan translation

--- validator/bitcoinsv_test.go
+++ validator/bitcoinsv_test.go
@@ -10,4 +10,6 @@ import (
 func TestBitcoinSVValidateAddress(t *testing.T) {
 	validator := &BitcoinSV{}
 
-	var mainnetC
+	var mainnetCases = map[string]*Result{
+		"1HX2swQNH9ezE8RagPPAaSBTcK3in9xWYF": {Success, true, P2PKH, ""},
+		"1NQhfGtWRwU6zg5G58TfQibHyJEuo6ZYXw": {Success, tru
