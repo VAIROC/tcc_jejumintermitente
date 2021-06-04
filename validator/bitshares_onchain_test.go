04adb52e0551c663692e2597c820a2f0a2ab7f59
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-06-04 20:01:34 +0000

    resolving merge conflict

--- validator/bitshares_onchain_test.go
+++ validator/bitshares_onchain_test.go
@@ -14,4 +14,7 @@ func TestBitsharesValidateAddress(t *testing.T) {
 	client := BitsharesClient{Endpoint: os.Getenv("AVA_BITSHARES_ENDPOINT")}
 	validator := &Bitshares{Client: &client}
 
-	var cases =
+	var cases = map[string]*Result{
+		"zbbts001":             {Success, true, Normal, ""},
+		"beos.gateway":         {Success, true, Normal, ""},
+		"huobi-bts-withdrawal": {Success, t
