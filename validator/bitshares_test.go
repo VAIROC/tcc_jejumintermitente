fae615fc00888d078b7c8b09269302a6faea680b
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-06-20 06:24:22 +0000

    Imported folder structure

--- validator/bitshares_test.go
+++ validator/bitshares_test.go
@@ -3,4 +3,13 @@ package validator
 import (
 	"testing"
 
-	"github.com/stretchr/testify/ass
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBitsharesIsAddressFormatValid(t *testing.T) {
+	validator := &Bitshares{}
+
+	var validCases = []string{
+		"zbbts001",
+		"beos.gateway",
+		
