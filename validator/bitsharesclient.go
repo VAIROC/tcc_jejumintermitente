334f00dfd1d29f6112a5b3402cf961a2970a86ae
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-06-27 18:52:15 +0000

    - updated readme

--- validator/bitsharesclient.go
+++ validator/bitsharesclient.go
@@ -12,4 +12,11 @@ type BitsharesClient struct {
 // GetAccount ...
 func (c *BitsharesClient) GetAccount(addr string) (AddressType, error) {
 	reqBody := map[string]interface{}{
-	
+		"method":  "get_account",
+		"params":  []string{addr},
+		"jsonrpc": "2.0",
+		"id":      1,
+	}
+	resp, err := httpclient.Post(c.Endpoint, reqBody)
+	if err != nil {
+		r
