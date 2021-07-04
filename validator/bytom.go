01ff7a4f55f5c4c29a9950e685c00db54d25cc19
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-07-04 20:56:28 +0000

    update README.rdoc

--- validator/bytom.go
+++ validator/bytom.go
@@ -6,4 +6,6 @@ type Bytom struct{}
 var _ Validator = (*Bytom)(nil)
 var _ SegwitAddress = (*Bytom)(nil)
 
-// ValidateAddress returns validate result o
+// ValidateAddress returns validate result of bytom address
+func (v *Bytom) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := SegwitAddrType(v, addr, network); addrType != Unknown {
