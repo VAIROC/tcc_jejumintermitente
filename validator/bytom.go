691138a9ea4b0bd1dc04e30957a324da9179a93e
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-07-04 10:50:17 +0000

    Update githubmarkdown.md

--- validator/bytom.go
+++ validator/bytom.go
@@ -1,1 +1,9 @@
-package validato
+package validator
+
+// Bytom ...
+type Bytom struct{}
+
+var _ Validator = (*Bytom)(nil)
+var _ SegwitAddress = (*Bytom)(nil)
+
+// ValidateAddress returns validate result o
