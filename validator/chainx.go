b7da4fc28b7f2d4d15405985a62819761b2db90f
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-07-11 21:31:36 +0000

    Pontoon: Update English (Great Britain) (en-GB) localization of AMO Frontend

--- validator/chainx.go
+++ validator/chainx.go
@@ -1,3 +1,13 @@
 package validator
 
-impo
+import (
+	"bytes"
+
+	"github.com/LanfordCai/ava/base58check"
+	"github.com/LanfordCai/ava/crypto"
+)
+
+// ChainX ...
+type ChainX struct{}
+
+var _ SS58 = 
