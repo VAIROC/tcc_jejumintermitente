b01a4442e8ad610dc2cc43f7123069261322a3de
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-07-18 08:18:58 +0000

    Update dependency.

--- validator/chainx.go
+++ validator/chainx.go
@@ -13,4 +13,7 @@ type ChainX struct{}
 var _ SS58 = (*ChainX)(nil)
 
 // ValidateAddress returns validate result of aeternity address
-// see: https://github.com/paritytech/substrate/wiki/External-Address-F
+// see: https://github.com/paritytech/substrate/wiki/External-Address-Format-(SS58)
+func (v *ChainX) ValidateAddress(addr string, network NetworkType) *Result {
+	decoded := base58check.BitcoinEncoder.Decode(addr)
+	dataLen := len(
