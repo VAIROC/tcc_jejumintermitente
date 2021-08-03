2cb1f4fb0d35d025e02c8cb82e1b38701e313063
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-08-03 21:00:56 +0000

    Update test expectations.

--- validator/chainx_test.go
+++ validator/chainx_test.go
@@ -12,4 +12,6 @@ func TestChainXValidateAddress(t *testing.T) {
 
 	var validCases = map[string]*Result{
 		"5QxFTz6oeBbgPtGMg531zUZ4JJz2RkbiCjkUu9z1Uf7Wi92d": {Success, true, Normal, ""},
-		"5SdW3TbRpnsbvTDa9QDXftM8mt
+		"5SdW3TbRpnsbvTDa9QDXftM8mtdw86ou8Z1Lyd3D4XgjSEii": {Success, true, Normal, ""},
+		"5QbeyxMUMhEeukBaxAqHip9E1u93kMqxf2dTBPDrPeY52B9o": {Success, true, Normal, ""},
+		"5R7sxzEYGGSGSCQd3DQLy
