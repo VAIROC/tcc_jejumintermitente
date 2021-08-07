3fc7a24694c3817aeb89b7664aeb7f71b13bd613
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-08-07 20:55:53 +0000

    Update MainDlg.cpp

--- validator/chainx_test.go
+++ validator/chainx_test.go
@@ -14,4 +14,6 @@ func TestChainXValidateAddress(t *testing.T) {
 		"5QxFTz6oeBbgPtGMg531zUZ4JJz2RkbiCjkUu9z1Uf7Wi92d": {Success, true, Normal, ""},
 		"5SdW3TbRpnsbvTDa9QDXftM8mtdw86ou8Z1Lyd3D4XgjSEii": {Success, true, Normal, ""},
 		"5QbeyxMUMhEeukBaxAqHip9E1u93kMqxf2dTBPDrPeY52B9o": {Success, true, Normal, ""},
-		"5R7sxzEYGGSGSCQd3DQLy
+		"5R7sxzEYGGSGSCQd3DQLyYwkA29jYGYaecHUHCsQ8s1Vxeo7": {Success, true, Normal, ""},
+		"5VQLcbTNn6UPYqLK87kPBHQvhvYL24z9bCa9XBtkkWekGrwj": {Success, true, Normal, ""},
+		"5UBLKqQ7e
