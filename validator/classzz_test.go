1831498af781bbe8239cb789a3201af28d4f70bc
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-09-03 18:16:44 +0000

    new commit

--- validator/classzz_test.go
+++ validator/classzz_test.go
@@ -11,4 +11,5 @@ func TestClasszzValidateAddress(t *testing.T) {
 	validator := &Classzz{}
 
 	var mainnetCases = map[string]*Result{
-		"cp283r0gjwd7lre962r32prf
+		"cp283r0gjwd7lre962r32prfyhqz0l0l6shwmzn7st":         {Success, true, CashAddrP2PKH, ""},
+		"cp2xxd7q5wzjxkjmvy6jshh2rz3eq5sjey7ja09mwx":         {Suc
