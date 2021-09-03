4feacf5852572f77fbe8e079a329e9f980609630
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-09-03 08:12:56 +0000

    Update main-1.6.css

--- validator/classzz_test.go
+++ validator/classzz_test.go
@@ -4,4 +4,11 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/stretchr/
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClasszzValidateAddress(t *testing.T) {
+	validator := &Classzz{}
+
+	var mainnetCases = map[string]*Result{
+		"cp283r0gjwd7lre962r32prf
