908db6833ff17d81237f24fa73a881ff085e04e8
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-09-08 19:27:56 +0000

    Fix typo in previous commit.

--- validator/classzz_test.go
+++ validator/classzz_test.go
@@ -14,4 +14,8 @@ func TestClasszzValidateAddress(t *testing.T) {
 		"cp283r0gjwd7lre962r32prfyhqz0l0l6shwmzn7st":         {Success, true, CashAddrP2PKH, ""},
 		"cp2xxd7q5wzjxkjmvy6jshh2rz3eq5sjey7ja09mwx":         {Success, true, CashAddrP2PKH, ""},
 		"classzz:cp2sn5ws3u5epfmvuc4tu5szf32gj78dnqekvpk4mp": {Success, true, CashAddrP2PKH, ""},
-		"classzz:cp2ts8jxwwadkc3dgqp08uk29tcmqvsj
+		"classzz:cp2ts8jxwwadkc3dgqp08uk29tcmqvsjzsh7jevgfm": {Success, true, CashAddrP2PKH, ""},
+	}
+
+	for addr, result := range mainnetCases {
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mainnet), r
