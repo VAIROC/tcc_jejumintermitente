843d09b34ce294897254018d494a7ad1dadc2c4a
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-04 07:16:58 +0000

    :memo: update docs

--- validator/cosmos.go
+++ validator/cosmos.go
@@ -3,4 +3,8 @@ package validator
 // Cosmos ...
 type Cosmos struct{}
 
-var _ Validator = (*Cosmos
+var _ Validator = (*Cosmos)(nil)
+var _ Bech32Address = (*Cosmos)(nil)
+
+// ValidateAddress returns validate result of cosmos address
+func (v *Cosmos) ValidateAd
