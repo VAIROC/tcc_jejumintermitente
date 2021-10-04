226fa0526cac5aec793cd619fc185e0778f889bf
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-04 09:42:59 +0000

    fix phpunit

--- validator/cosmos.go
+++ validator/cosmos.go
@@ -7,4 +7,9 @@ var _ Validator = (*Cosmos)(nil)
 var _ Bech32Address = (*Cosmos)(nil)
 
 // ValidateAddress returns validate result of cosmos address
-func (v *Cosmos) ValidateAd
+func (v *Cosmos) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := Bech32AddrType(v, addr, network); addrType != Unknown {
+		return &Result{Success, true, Normal, ""}
+	}
+
+	r
