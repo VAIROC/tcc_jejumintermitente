c778d73fc6778f12a66389790c8586d7650ccdc1
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-13 13:05:47 +0000

    arm64: __clear_user: handle exceptions on strb

--- validator/dash.go
+++ validator/dash.go
@@ -8,4 +8,11 @@ var _ BitcoinLike = (*Dash)(nil)
 // ValidateAddress returns validate result of dash address
 func (v *Dash) ValidateAddress(addr string, network NetworkType) *Result {
 	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
-		return &Result{Success, 
+		return &Result{Success, true, addrType, ""}
+	}
+
+	return &Result{Success, false, Unknown, ""}
+}
+
+// AddressVersion returns dash address version according to the address type and
+// networ
