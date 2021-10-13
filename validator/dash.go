ad00857bad99167ca64c7d393019fa85c18b8d66
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-13 10:03:48 +0000

    Updated Turkish Translation

--- validator/dash.go
+++ validator/dash.go
@@ -6,4 +6,6 @@ type Dash struct{}
 var _ BitcoinLike = (*Dash)(nil)
 
 // ValidateAddress returns validate result of dash address
-fun
+func (v *Dash) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
+		return &Result{Success, 
