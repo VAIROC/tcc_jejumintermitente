cd468119d6fe352eea088090a12c18157de4ef17
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-16 06:26:49 +0000

    Comment update

--- validator/dash_test.go
+++ validator/dash_test.go
@@ -7,3 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDashValidateAddress(t *testing.T) {
+	validator := &Dash{}
+
+	var mainnetCases = map[string]*Result{
+		"XpMfHazu1y8bgnP8csW73LWULGKgs4XH6U": {
