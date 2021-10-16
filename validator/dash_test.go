63e5a7f2ec1d1f630d54096a74a5d36ecf943fa8
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-16 07:02:49 +0000

    ui cleanup

--- validator/dash_test.go
+++ validator/dash_test.go
@@ -11,4 +11,6 @@ func TestDashValidateAddress(t *testing.T) {
 	validator := &Dash{}
 
 	var mainnetCases = map[string]*Result{
-		"XpMfHazu1y8bgnP8csW73LWULGKgs4XH6U": {
+		"XpMfHazu1y8bgnP8csW73LWULGKgs4XH6U": {Success, true, P2PKH, ""},
+		"Xdk9TLNrELrM4jpu6m15zZvurG66sZKi6C": {Success, true, P2PKH, ""},
+		"XripctRDfASrofg4BKco41Zqvm1KaCdQpk": {Success,
