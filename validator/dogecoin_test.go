89e39fc699ba6d8b809485911df2864f7a539f75
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-28 17:24:20 +0000

    ext4: avoid kmemcheck complaint from reading uninitialized memory

--- validator/dogecoin_test.go
+++ validator/dogecoin_test.go
@@ -13,4 +13,8 @@ func TestDogecoinValidateAddress(t *testing.T) {
 	var mainnetCases = map[string]*Result{
 		"D8osUgxgU4J2yEhCzN5xQQ42tuuG7qBdri": {Success, true, P2PKH, ""},
 		"DFwtUPqF3ornMxgs6gt6A3Rpcuwizzsnev": {Success, true, P2PKH, ""},
-		"DCZHAVah2dCyD1vmjn
+		"DCZHAVah2dCyD1vmjnUm6t7Aao4YwtYWkM": {Success, true, P2PKH, ""},
+		"A6T5s5B1Saja9yAJWxQN8SNXvNzxaoFdcy": {Success, true, P2SH, ""},
+	}
+
+	for addr, result := ran
