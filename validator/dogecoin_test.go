ba7f1bb9a28a611bf9d62e9251ca6c0b69dfa472
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-28 16:16:45 +0000

    sched: Remove one division operation in find_busiest_queue()

--- validator/dogecoin_test.go
+++ validator/dogecoin_test.go
@@ -2,3 +2,13 @@ package validator
 
 import (
 	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDogecoinValidateAddress(t *testing.T) {
+	validator := &Dogecoin{}
+
+	var mainnetCases = map[string]*Result{
+		"D8
