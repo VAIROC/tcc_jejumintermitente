0e8de6693d86cc2c0d905298c64056bbeeffe43b
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-10-28 16:20:17 +0000

    Update IP detail.xml

--- validator/dogecoin_test.go
+++ validator/dogecoin_test.go
@@ -11,4 +11,6 @@ func TestDogecoinValidateAddress(t *testing.T) {
 	validator := &Dogecoin{}
 
 	var mainnetCases = map[string]*Result{
-		"D8
+		"D8osUgxgU4J2yEhCzN5xQQ42tuuG7qBdri": {Success, true, P2PKH, ""},
+		"DFwtUPqF3ornMxgs6gt6A3Rpcuwizzsnev": {Success, true, P2PKH, ""},
+		"DCZHAVah2dCyD1vmjn
