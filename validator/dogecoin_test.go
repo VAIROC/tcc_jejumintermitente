e751e000595d16a88aaec5f9d075e54aefb4538d
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-11-03 22:50:30 +0000

    Update travis yml

--- validator/dogecoin_test.go
+++ validator/dogecoin_test.go
@@ -26,4 +26,8 @@ func TestDogecoinValidateAddress(t *testing.T) {
 		"nYF8DkhE3orcq7CypP1oEQTWWcG1yYkqec":  {Success, true, P2PKH, ""},
 		"nXiLZ1g1xrcLwYq1s9wVCdiewLeY7Bw1X3":  {Success, true, P2PKH, ""},
 		"nj4xWxERcYpXQLZf61R5xzMDfTjv72ajJh":  {Success, true, P2PKH, ""},
-		"2NDutUP
+		"2NDutUPDhfMYGUFPhHRy4ZRraHVrEK7odr6": {Success, true, P2SH, ""},
+		"2MwU3UtzeHG59xV7J7VSoM42U7qwCJ1GSJz": {Success, true, P2SH, ""},
+	}
+
+	for addr, re
