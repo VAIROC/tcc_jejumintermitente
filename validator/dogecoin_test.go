b489bc88f8ded493be8a873ae5358e0a872d5e79
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-11-19 18:35:22 +0000

    0.0.36

--- validator/dogecoin_test.go
+++ validator/dogecoin_test.go
@@ -44,4 +44,5 @@ func TestDogecoinValidateAddress(t *testing.T) {
 		"GEzKoZ59mhmpMzjNBWNoYKvLhFLAdHuL6P":                             {Success, false, Unknown, ""},
 		"bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvze":  {Success, false, Unknown, ""},
 		"bc1qf2epzuxpm32t4g02m9ya2a3lcphqg8kzp8mhchgjedg2w4n4300s0057u5": {Success, false, Unknown, ""},
-		"bb1qql2qamp2az7h5
+		"bb1qql2qamp2az7h5ejnjyuxt4294watgcmrd76n8c":                     {Success, false, Unknown, ""},
+		"bc1q3l9k4lm5z4mtsl6smmj9qxy03e65x3maz4p9xv":                     {Success, false, Unknown, 
