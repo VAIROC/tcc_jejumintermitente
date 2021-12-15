0c8d57f86bb156d5d4687931f23758d6ce5ab992
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-12-15 20:10:07 +0000

    Add manifest file

--- validator/ethereum.go
+++ validator/ethereum.go
@@ -17,4 +17,10 @@ var _ Prefixer = (*Ethereum)(nil)
 
 // ValidateAddress - Check an Ethereum address is valid or not
 func (e *Ethereum) ValidateAddress(addr string, network NetworkType) *Result {
-	if !e.isValidUn
+	if !e.isValidUncheckedAddress(addr) {
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	if !e.withChecksum(addr) {
+		return &Result{Success, true, HexWithoutChecksum, ""}
+	}
