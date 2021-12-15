6eeac05794fe55f18682b0cc63eab214e7227df0
VAIROC <123872636+VAIROC@users.noreply.github.com>
2021-12-15 05:59:21 +0000

    Add credit

--- validator/ethereum.go
+++ validator/ethereum.go
@@ -4,3 +4,15 @@ import (
 	"bytes"
 	"encoding/hex"
 	"strings"
+	"unicode"
+
+	"github.com/LanfordCai/ava/crypto"
+)
+
+// Ethereum ..
+type Ethereum struct{}
+
+var _ Validator = (*Ethereum)(nil)
+var _ Prefixer = (*Ethereum)(nil)
+
+// Validat
