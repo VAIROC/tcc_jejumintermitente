534033c6ac7d74a818924a67a17cec54813ee3f8
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-01-17 07:08:05 +0000

    beta 1

--- validator/filecoin_test.go
+++ validator/filecoin_test.go
@@ -19,4 +19,5 @@ func TestFilecoinValidateAddress(t *testing.T) {
 		"f01024":   {Success, true, FilID, ""},
 		"f01729":   {Success, true, FilID, ""},
 		"f0999999": {Success, true, FilID, ""},
-		"f15ihq5ibzwki2b4ep2f46avlkrqzhpqgtga7pdrq":                        
+		"f15ihq5ibzwki2b4ep2f46avlkrqzhpqgtga7pdrq":                                              {Success, true, FilSecp256k1, ""},
+		"f12fiakbhe2gwd5cnmrenekasyn6v5tnaxaqizq6a":                                              {Success, tr
