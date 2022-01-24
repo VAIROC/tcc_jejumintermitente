f85a35cc939128de4e38e42a5948ed91bcf59e20
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-01-24 10:35:07 +0000

    fixed links in README

--- validator/filecoin_test.go
+++ validator/filecoin_test.go
@@ -32,3 +32,5 @@ func TestFilecoinValidateAddress(t *testing.T) {
 		"f2gfvuyh7v2sx3patm5k23wdzmhyhtmqctasbr23y":                                              {Success, true, FilActor, ""},
 		"f3vvmn62lofvhjd2ugzca6sof2j2ubwok6cj4xxbfzz4yuxfkgobpihhd2thlanmsh3w2ptld2gqkn2jvlss4a": {Success, true, FilBLS, ""},
 		"f3wmuu6crofhqmm3v4enos73okk2l366ck6yc4owxwbdtkmpk42ohkqxfitcpa57pjdcftql4tojda2poeruwa": {Success, true, FilBLS, ""},
+		"f3s2q2hzhkpiknjgmf4zq3ejab2rh62qbndueslmsdzervrhapxr7dftie4kpnpdiv2n6tvkr743ndhrsw6d3a": {Success, true, FilBLS, ""},
+		"f3q22fijmmlckhl56rn5nkyamkph3mcfu
