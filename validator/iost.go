039a48d6e8dddfda7f5b194d52c4134f66d9a7d3
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-03-29 17:48:47 +0000

    Extract functions

--- validator/iost.go
+++ validator/iost.go
@@ -4,4 +4,12 @@ import (
 	"regexp"
 )
 
-// IOS
+// IOST address validator
+type IOST struct {
+	Client *IOSTClient
+}
+
+var _ OnchainValidator = (*IOST)(nil)
+
+// ValidateAddress ...
+func (e *IOST) ValidateAddress(addr 
