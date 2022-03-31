ca6fc38773c3b48c9efd55bc4bfc56d0cd4bfea9
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-03-31 08:19:32 +0000

    [ci skip] ongoing documentation work

--- validator/iost.go
+++ validator/iost.go
@@ -14,4 +14,13 @@ var _ OnchainValidator = (*IOST)(nil)
 // ValidateAddress ...
 func (e *IOST) ValidateAddress(addr string, network NetworkType) *Result {
 	if isValid := e.IsAddressFormatValid(addr, network); !isValid {
-		return &Result{Success, false, Unkno
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	addrType, err := e.Client.GetAccount(addr)
+	if err != nil {
+		return &Result{Failure, false, Unknown, err.Error()}
+	}
+
+	if addrType == Unknown {
+		
