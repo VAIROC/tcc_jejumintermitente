1412170f74242f8117bddaa1ff6670a55f3b7fa0
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-04-05 14:37:38 +0000

    convenience methods

--- validator/iost_onchain_test.go
+++ validator/iost_onchain_test.go
@@ -1,2 +1,14 @@
 // +build !local
 
+package validator
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIOSTValidateAddress(t *testing.T) {
+	client := IOSTClient{Endpoint
