529b7012a3a914fe5145e87e4aca6ae29946857f
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-04-10 21:05:34 +0000

    add wercker.yml

--- validator/iost_test.go
+++ validator/iost_test.go
@@ -1,1 +1,12 @@
-package va
+package validator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIOSTIsAddressFormatValid(t *testing.T) {
+	validator := &IOST{}
+
+	var validCas
