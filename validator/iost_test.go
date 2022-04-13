b343cb0f6e86a7ea66ca5f0ce52ce33aa0fa1856
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-04-13 05:30:28 +0000

    missing image

--- validator/iost_test.go
+++ validator/iost_test.go
@@ -9,4 +9,14 @@ import (
 func TestIOSTIsAddressFormatValid(t *testing.T) {
 	validator := &IOST{}
 
-	var validCas
+	var validCases = []string{
+		"kainiost",
+		"ukay66",
+		"huobi_iost",
+		"13177913280",
+		"_iost",
+		"y2a_yfec2st",
+	}
+
+	for _, addr := range validCases {
+		assert.True(t, valida
