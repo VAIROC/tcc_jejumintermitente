2b0890578ccc0241f8c44df087973f304b48f1f6
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-04-20 17:20:52 +0000

    Delete css

--- validator/iostclient.go
+++ validator/iostclient.go
@@ -16,4 +16,12 @@ type IOSTClient struct {
 
 // GetAccount ...
 func (c *IOSTClient) GetAccount(addr string) (AddressType, error) {
-	url := fmt.Sprintf("%s/getAccount/%s/false"
+	url := fmt.Sprintf("%s/getAccount/%s/false", c.Endpoint, addr)
+	resp, err := httpclient.Get(url)
+	if err != nil {
+		return Unknown, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		return Un
