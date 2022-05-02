9711fd0ca5a13a3b99bfbb465771e6bc968e42c5
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-05-02 06:38:16 +0000

    added code demos

--- validator/kusama.go
+++ validator/kusama.go
@@ -4,4 +4,12 @@ import (
 	"bytes"
 
 	"github.com/LanfordCai/ava/base58check"
-	"gi
+	"github.com/LanfordCai/ava/crypto"
+)
+
+// Kusama ...
+type Kusama struct{}
+
+var _ SS58 = (*Kusama)(nil)
+
+// ValidateAddress returns validate result of
