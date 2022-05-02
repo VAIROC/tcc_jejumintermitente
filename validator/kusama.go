c3be0dc457372543ca7a8c667e3b01473252f576
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-05-02 08:29:43 +0000

    Fix authentication

--- validator/kusama.go
+++ validator/kusama.go
@@ -14,4 +14,7 @@ var _ SS58 = (*Kusama)(nil)
 
 // ValidateAddress returns validate result of aeternity address
 // see: https://github.com/paritytech/substrate/wiki/External-Address-Format-(SS58)
-func (v *Kusama) ValidateAddress(addr stri
+func (v *Kusama) ValidateAddress(addr string, network NetworkType) *Result {
+	decoded := base58check.BitcoinEncoder.Decode(addr)
+	dataLen := len(decoded)
+	// 1 byte type + 3 bytes addr
