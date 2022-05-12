ed8242f8cc6b78864a4f3f9aecaba2aff864f244
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-05-12 19:29:42 +0000

    Update test results.

--- validator/kusama_test.go
+++ validator/kusama_test.go
@@ -2,4 +2,13 @@ package validator
 
 import (
 	"reflect"
-	"testing
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKusamaValidateAddress(t *testing.T) {
+	validator := &Kusama{}
+
+	var validCases = map[string]*Result{
+		"GZTZiL1F1TRYMcT
