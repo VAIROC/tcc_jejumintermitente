c40a03d63bf86c1cda1b9d3fb5b3bfdec677e8f2
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-05-14 07:45:41 +0000

    star

--- validator/kusama_test.go
+++ validator/kusama_test.go
@@ -11,4 +11,6 @@ func TestKusamaValidateAddress(t *testing.T) {
 	validator := &Kusama{}
 
 	var validCases = map[string]*Result{
-		"GZTZiL1F1TRYMcT
+		"GZTZiL1F1TRYMcT7mmkUiujR6R2oVcw7bsWpWawwQJuRzdD": {Success, true, Normal, ""},
+		"HEVQFCv5HWcwZhzJEX93gQfXikJQ9VgfHoQ9RvXk3KYHN29": {Success, true, Normal, ""},
+		"Ek5FeX
