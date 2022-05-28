be65e525ddd7acd9e55940b2dd96f66b57d753c9
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-05-28 13:09:19 +0000

    Fix example code

--- validator/kusama_test.go
+++ validator/kusama_test.go
@@ -35,4 +35,7 @@ func TestKusamaValidateAddress(t *testing.T) {
 	}
 
 	for addr, result := range invalidCases {
-		assert.True(t, re
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mainnet), result), addr)
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Testnet), result), addr)
+	}
+}
