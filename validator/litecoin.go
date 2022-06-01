e2bede4884825f7900bf2532fdeb9c6dd0b93686
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-01 14:02:15 +0000

    removed image

--- validator/litecoin.go
+++ validator/litecoin.go
@@ -6,4 +6,9 @@ type Litecoin struct{}
 var _ BitcoinLike = (*Litecoin)(nil)
 
 // ValidateAddress returns validate result of litecoin address
-func (v *Litecoin) ValidateAddress(addr string, network NetworkType) *Result
+func (v *Litecoin) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
+		return &Result{Success, true, addrType, ""}
+	}
+
+	return &Result{Su
