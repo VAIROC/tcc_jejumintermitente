8b0c6687c9a24a8da56f3e1f5784b88ef5132c18
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-01 13:15:52 +0000

    Updated author

--- validator/litecoin.go
+++ validator/litecoin.go
@@ -3,4 +3,7 @@ package validator
 // Litecoin ...
 type Litecoin struct{}
 
-var _ BitcoinLike = (*Liteco
+var _ BitcoinLike = (*Litecoin)(nil)
+
+// ValidateAddress returns validate result of litecoin address
+func (v *Litecoin) ValidateAddress(addr string, network NetworkType) *Result
