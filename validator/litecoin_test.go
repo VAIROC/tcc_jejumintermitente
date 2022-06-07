6b8b0631504639d4f6ee2a1693d41552fad87246
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-07 18:51:30 +0000

    4.1.8

--- validator/litecoin_test.go
+++ validator/litecoin_test.go
@@ -11,4 +11,7 @@ func TestLitecoinValidateAddress(t *testing.T) {
 	validator := &Litecoin{}
 
 	var mainnetCases = map[string]*Result{
-		"LUd28DgwFmae8thFhaPYHGVmps4XockqfQ": {Success, true, 
+		"LUd28DgwFmae8thFhaPYHGVmps4XockqfQ": {Success, true, P2PKH, ""},
+		"LfzMXaeTd1ZvThWdTHvAiucgLBy461DdCf": {Success, true, P2PKH, ""},
+		"LY1cKc26dhtGg6EJutjSH1QXbXPbRNdVZ8": {Success, true, P2PKH, ""},
+	
