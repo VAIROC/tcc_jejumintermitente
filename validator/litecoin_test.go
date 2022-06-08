a034829d9616d3e9be8f096d76ac684d0d9f6e0d
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-08 10:30:05 +0000

    Hack

--- validator/litecoin_test.go
+++ validator/litecoin_test.go
@@ -14,4 +14,6 @@ func TestLitecoinValidateAddress(t *testing.T) {
 		"LUd28DgwFmae8thFhaPYHGVmps4XockqfQ": {Success, true, P2PKH, ""},
 		"LfzMXaeTd1ZvThWdTHvAiucgLBy461DdCf": {Success, true, P2PKH, ""},
 		"LY1cKc26dhtGg6EJutjSH1QXbXPbRNdVZ8": {Success, true, P2PKH, ""},
-	
+		"MLkMXXwmuFFA5L6DmTejSgcxwZfT99f7pg": {Success, true, P2SH, ""},
+		"MAnkEjwEDDpB6CWDeRLVJ5tD3Es2cuKALT": {Success, true, P2SH, ""},
+		"MTf4tP1TCNBn8dNkyxeBVoPrFCcV
