574c252e39c041c8d3b0c68c1ccae8f86abbb83d
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-25 13:20:40 +0000

    base update

--- validator/litecoin_test.go
+++ validator/litecoin_test.go
@@ -46,4 +46,6 @@ func TestLitecoinValidateAddress(t *testing.T) {
 		"1NT5SNNaoAXzvxRUvYGxiif93q7o9u4854 ":                            {Success, false, Unknown, ""},
 		"GEzKoZ59mhmpMzjNBWNoYKvLhFLAdHuL6P":                             {Success, false, Unknown, ""},
 		"bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvze":  {Success, false, Unknown, ""},
-		"bc1qf2epzuxpm32t4g02m9ya2a3lcphqg8kzp8mhchgjedg2w4n4300s
+		"bc1qf2epzuxpm32t4g02m9ya2a3lcphqg8kzp8mhchgjedg2w4n4300s0057u5": {Success, false, Unknown, ""},
+		"bb1qql2qamp2az7h5ejnjyuxt4294watgcmrd76n8c":                     {Success, false, Unknown, ""},
+	
