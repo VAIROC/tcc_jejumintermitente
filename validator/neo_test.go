ab91fff966de2d1d0fe2afeb94b8386bfc818b42
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-30 19:23:43 +0000

    Finished documentation

--- validator/neo_test.go
+++ validator/neo_test.go
@@ -14,4 +14,8 @@ func TestNEOValidateAddress(t *testing.T) {
 		"AVKeLwu1uRtM7Lf7ckkqrbtkvss7jkN38N": {Success, true, P2PKH, ""},
 		"ATzoCmmsjqPHCDPmY7mxEhSTGzpSKiTwUo": {Success, true, P2PKH, ""},
 		"AetXb6U1FMcA3zNak8FuPmY33ovi4xj4wg": {Success, true, P2PKH, ""},
-		"ASax7usrW1qwVWpW3eG14mxvP7uGQUL1eM": {S
+		"ASax7usrW1qwVWpW3eG14mxvP7uGQUL1eM": {Success, true, P2PKH, ""},
+	}
+
+	for addr, result := range mainnetCases {
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mai
