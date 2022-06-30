834ece234979b00fa2c5b83130760c1697384860
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-06-30 10:19:31 +0000

    fec_mpc52xx: fix timestamp filtering

--- validator/neo_test.go
+++ validator/neo_test.go
@@ -12,4 +12,6 @@ func TestNEOValidateAddress(t *testing.T) {
 
 	var mainnetCases = map[string]*Result{
 		"AVKeLwu1uRtM7Lf7ckkqrbtkvss7jkN38N": {Success, true, P2PKH, ""},
-		"ATzoCmmsjqPHCDPmY7mxEhSTGzpSK
+		"ATzoCmmsjqPHCDPmY7mxEhSTGzpSKiTwUo": {Success, true, P2PKH, ""},
+		"AetXb6U1FMcA3zNak8FuPmY33ovi4xj4wg": {Success, true, P2PKH, ""},
+		"ASax7usrW1qwVWpW3eG14mxvP7uGQUL1eM": {S
