e7038cf42e76513373b41775e3bc10cb78e1b76f
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-07-24 14:45:57 +0000

    Whitespace only

--- validator/oasis_onchain_test.go
+++ validator/oasis_onchain_test.go
@@ -11,4 +11,8 @@ import (
 )
 
 func TestOasisValidateAddress(t *testing.T) {
-	client := RosettaClient{Endpoint: os.Getenv("AVA_OASIS
+	client := RosettaClient{Endpoint: os.Getenv("AVA_OASIS_ENDPOINT")}
+	validator := &Oasis{Client: &client}
+
+	var cases = map[string]*Result{
+		"oasis1qqs6ylpfurhf6gc9mw232fkmrt3d0673lyzc5xf2": {Success, true, Norm
