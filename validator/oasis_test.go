961506792dca1d31fbd7efa2c67820136c4620b0
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-07-29 18:13:48 +0000

    Bump up package version

--- validator/oasis_test.go
+++ validator/oasis_test.go
@@ -1,1 +1,12 @@
-package va
+package validator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOasisIsAddressFormatValid(t *testing.T) {
+	validator := &Oasis{}
+
+	var validCases = [
