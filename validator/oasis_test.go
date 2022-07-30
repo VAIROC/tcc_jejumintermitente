cae8c20d1217f2bebc9f0b5e561390b3d31c87b9
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-07-30 15:30:37 +0000

    initial project creation

--- validator/oasis_test.go
+++ validator/oasis_test.go
@@ -9,4 +9,7 @@ import (
 func TestOasisIsAddressFormatValid(t *testing.T) {
 	validator := &Oasis{}
 
-	var validCases = [
+	var validCases = []string{
+		"oasis1qqs6ylpfurhf6gc9mw232fkmrt3d0673lyzc5xf2",
+		"oasis1qrad7s7nqm4gvyzr8yt2rdk0ref489rn3vn400d6",
+		"oasis1qp29h8ykmxet46eqzw0wennrmm
