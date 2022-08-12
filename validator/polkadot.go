d7884dae17fe9904ab6e808c6e4bfa76f3207eec
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-08-12 13:55:18 +0000

    Updated Spanish Translation

--- validator/polkadot.go
+++ validator/polkadot.go
@@ -1,1 +1,13 @@
-package validato
+package validator
+
+import (
+	"bytes"
+
+	"github.com/LanfordCai/ava/base58check"
+	"github.com/LanfordCai/ava/crypto"
+)
+
+// Polkadot ...
+type Polkadot struct{}
+
+var _ SS58 = (*Polkadot)(n
