1a1313d84f6887af8f2ca05fae42c3b84a1bbec9
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-08-13 05:57:51 +0000

    Update for Chef 15 license agreement and Chef Workstation

--- validator/polkadot.go
+++ validator/polkadot.go
@@ -10,4 +10,8 @@ import (
 // Polkadot ...
 type Polkadot struct{}
 
-var _ SS58 = (*Polkadot)(n
+var _ SS58 = (*Polkadot)(nil)
+
+// ValidateAddress returns validate result of aeternity address
+// see: https://github.com/paritytech/substrate/wiki/External-Address-Format-(SS58)
+func (v *P
