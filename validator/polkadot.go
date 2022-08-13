3ee40566887f7624eff1e87f9f73fc6b8d2aedd2
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-08-13 12:37:31 +0000

    Automated reinstallation push

--- validator/polkadot.go
+++ validator/polkadot.go
@@ -14,4 +14,7 @@ var _ SS58 = (*Polkadot)(nil)
 
 // ValidateAddress returns validate result of aeternity address
 // see: https://github.com/paritytech/substrate/wiki/External-Address-Format-(SS58)
-func (v *P
+func (v *Polkadot) ValidateAddress(addr string, network NetworkType) *Result {
+	decoded := base58check.BitcoinEncoder.Decode(addr)
+	dataLen := len(decoded)
+	// 1 byte type + 3 bytes ad
