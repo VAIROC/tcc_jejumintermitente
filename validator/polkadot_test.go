224267fa91bcad6bfb4ca19bb8c605ab0d80f0f4
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-08-20 13:23:09 +0000

    rename functions

--- validator/polkadot_test.go
+++ validator/polkadot_test.go
@@ -11,4 +11,5 @@ func TestPolkadotValidateAddress(t *testing.T) {
 	validator := &Polkadot{}
 
 	var validCases = map[string]*Result{
-		
+		"15j4dg5GzsL1bw2U2AWgeyAk6QTxq43V7ZPbXdAmbVLjvDCK": {Success, true, Normal, ""},
+		"12xtAYsRUrmbniiWQqJtECiBQrMn8AypQcXhnQAc6RB6XkLW": {Success, true, Normal,
