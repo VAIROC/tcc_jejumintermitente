2ec70f2a72eee68ded80806823495074c5e7f4a0
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-08-23 16:05:45 +0000

    bump to 0.4.1

--- validator/polkadot_test.go
+++ validator/polkadot_test.go
@@ -14,4 +14,6 @@ func TestPolkadotValidateAddress(t *testing.T) {
 		"15j4dg5GzsL1bw2U2AWgeyAk6QTxq43V7ZPbXdAmbVLjvDCK": {Success, true, Normal, ""},
 		"12xtAYsRUrmbniiWQqJtECiBQrMn8AypQcXhnQAc6RB6XkLW": {Success, true, Normal, ""},
 		"14Ns6kKbCoka3MS4Hn6b7oRw9fFejG8RH5rq5j63cWUfpPDJ": {Success, true, Normal, ""},
-		"16DGiP6jDwAfkAeqGfkUCtheKgUzTy7UeaiFFBAv8BwX3RhN": {Success, 
+		"16DGiP6jDwAfkAeqGfkUCtheKgUzTy7UeaiFFBAv8BwX3RhN": {Success, true, Normal, ""},
+		"1vTfju3zruADh7sbBznxWCpircNp9ErzJaPQZKyrUknApRu":  {Success, true, Normal, ""},
+		"1tZzPmcq8Auisttygmg9g6tPMtrh9i3b22D3tKXvde7ibRB":  {Success, true,
