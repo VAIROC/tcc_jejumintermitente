14594975c798a46e73770df4ac9dacd42e33f4b4
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-08-28 18:13:15 +0000

    Small fixups

--- validator/qtum.go
+++ validator/qtum.go
@@ -6,4 +6,6 @@ type Qtum struct{}
 var _ BitcoinLike = (*Qtum)(nil)
 
 // ValidateAddress returns validate result of qtum address
-func (v *Qtum) Validat
+func (v *Qtum) ValidateAddress(addr string, network NetworkType) *Result {
+	if addrType := NormalAddrType(v, addr, network); addrType != Unknown {
+		return &Result{Success, true, addrType, ""}
