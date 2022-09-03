4d010162732197a4a0ac64920f0448e166efe047
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-03 19:56:27 +0000

    v3.0.8

--- validator/qtum_test.go
+++ validator/qtum_test.go
@@ -4,4 +4,11 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/stretchr/tes
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQtumValidateAddress(t *testing.T) {
+	validator := &Qtum{}
+
+	var mainnetCases = map[string]*Result{
+		"QhzgLgC9DY
