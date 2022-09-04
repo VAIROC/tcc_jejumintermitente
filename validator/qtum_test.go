80d1644ca502478840dd23ac40e0b9fa0ef18484
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-04 14:14:26 +0000

    Update reinstall.sh

--- validator/qtum_test.go
+++ validator/qtum_test.go
@@ -13,4 +13,7 @@ func TestQtumValidateAddress(t *testing.T) {
 	var mainnetCases = map[string]*Result{
 		"QhzgLgC9DYxn9sTXWfcsFdKbFbbx2DKDL4": {Success, true, P2PKH, ""},
 		"QfFE2wjWdqPDX4TQESSdEyb5y9DqL5wYHA": {Success, true, P2PKH, ""},
-		"QNG4mkK4thjgMaFHEStfW7gseWaVhcy6QY":
+		"QNG4mkK4thjgMaFHEStfW7gseWaVhcy6QY": {Success, true, P2PKH, ""},
+		"MLkMXXwmuFFA5L6DmTejSgcxwZfT99f7pg": {Success, true, P2SH, ""},
+		"MAnkEjwEDDpB6CWDeRLVJ5tD3Es2cuKALT": {Success, true, P2SH, ""},
+		"
