5f4b5b77a900c2509e49c46013b8a45fb6b28bdc
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-04 11:16:38 +0000

    Automated Swig Perl5 file repair.

--- validator/qtum_test.go
+++ validator/qtum_test.go
@@ -11,4 +11,6 @@ func TestQtumValidateAddress(t *testing.T) {
 	validator := &Qtum{}
 
 	var mainnetCases = map[string]*Result{
-		"QhzgLgC9DY
+		"QhzgLgC9DYxn9sTXWfcsFdKbFbbx2DKDL4": {Success, true, P2PKH, ""},
+		"QfFE2wjWdqPDX4TQESSdEyb5y9DqL5wYHA": {Success, true, P2PKH, ""},
+		"QNG4mkK4thjgMaFHEStfW7gseWaVhcy6QY":
