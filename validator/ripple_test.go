ff2b573cf5953623e576094da7e5e431909c02c8
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-17 16:01:08 +0000

    changed project name

--- validator/ripple_test.go
+++ validator/ripple_test.go
@@ -8,4 +8,8 @@ import (
 )
 
 func TestRippleValidateAddress(t *testing.T) {
-	validator := &Ri
+	validator := &Ripple{}
+
+	var validCases = map[string]*Result{
+		"r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV": {Success, true, Normal, ""},
+		"rKLpjpCoXgLQQYQyj13zgay73rsgmzNH13": {Succe
