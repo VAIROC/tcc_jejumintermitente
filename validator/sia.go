00fb4a675359f3720f58c1a197e14bcfc34cb93a
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-24 10:33:54 +0000

    :airplane: Auto-commit: Bumping Terraform CLI version

--- validator/sia.go
+++ validator/sia.go
@@ -1,4 +1,14 @@
 package validator
 
 import (
-	"e
+	"encoding/hex"
+
+	"github.com/LanfordCai/ava/crypto"
+)
+
+// Sia ...
+type Sia struct{}
+
+var _ Validator = (*Sia)(nil)
+
+// ValidateAddress - Check a Sia addres
