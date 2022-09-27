f5190d6eb4bfa3bb972e4b426f91ac46392e5ba1
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-27 22:59:28 +0000

    Whatever.

--- validator/sia.go
+++ validator/sia.go
@@ -11,4 +11,7 @@ type Sia struct{}
 
 var _ Validator = (*Sia)(nil)
 
-// ValidateAddress - Check a Sia addres
+// ValidateAddress - Check a Sia address is valid or not
+func (s *Sia) ValidateAddress(addr string, network NetworkType) *Result {
+	unlockhashWithChecksum, err := hex.DecodeString(addr)
+	i
