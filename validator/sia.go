f0f332d24991448a2ce06a3246ae42519058990c
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-09-28 19:01:18 +0000

    Update version.cpp

--- validator/sia.go
+++ validator/sia.go
@@ -14,4 +14,9 @@ var _ Validator = (*Sia)(nil)
 // ValidateAddress - Check a Sia address is valid or not
 func (s *Sia) ValidateAddress(addr string, network NetworkType) *Result {
 	unlockhashWithChecksum, err := hex.DecodeString(addr)
-	i
+	if err != nil || len(unlockhashWithChecksum) != 38 {
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	unlockhash := unlockhashWithChecksum[:32]
+	checksum256 := crypto
