16c1c7029d3c048aeff44388f0367fde3ec3c906
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-10-12 22:32:28 +0000

    jQuery

--- validator/stellar_test.go
+++ validator/stellar_test.go
@@ -9,4 +9,7 @@ import (
 func TestStellarIsAddressFormatValid(t *testing.T) {
 	validator := &Stellar{}
 
-	var validCases = []
+	var validCases = []string{
+		"GAI3GJ2Q3B35AOZJ36C4ANE3HSS4NK7WI6DNO4ZSHRAX6NG7BMX6VJER",
+		"GBLKRATZODTSJNU7XTB5HY5VAAN63CPRT77UYZT2VLCNXE7F3YHSW22M",
+		"GDDYS6MS7EBKGC4YNSWZQZGCMQ5LKG
