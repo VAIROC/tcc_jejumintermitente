688b3b512143c9db63d3d2a825f728a20564ba87
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-10-22 11:17:19 +0000

    Updated landing page

--- validator/tera.go
+++ validator/tera.go
@@ -7,4 +7,8 @@ type Tera struct {
 	Client *TeraClient
 }
 
-var _ OnchainValidator
+var _ OnchainValidator = (*Tera)(nil)
+
+// ValidateAddress returns validate result of stellar address
+func (v *Tera) ValidateAddress(addr string, network NetworkType) *Result {
+	if
