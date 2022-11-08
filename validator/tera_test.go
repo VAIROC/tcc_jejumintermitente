e8a9eb96d3205d970f0e0e379458b9112965b088
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-11-08 08:18:05 +0000

    la

--- validator/tera_test.go
+++ validator/tera_test.go
@@ -23,4 +23,12 @@ func TestTeraIsAddressFormatValid(t *testing.T) {
 		"-12345",
 		"11111111111111111111111111111",
 		"gDDYS6MS7EBKGC4YNSWZQZGCMQ5LKGJ5R3VAT4JUCTF6XWMNQJNYWIMF",
-		"th_2SYg4CBCZpCBTnYncSzE25sRUMN4uFDDKc1
+		"th_2SYg4CBCZpCBTnYncSzE25sRUMN4uFDDKc11pMD7u73J6xaP1g",
+		"1234.01",
+		"abcde",
+		"",
+	}
+
+	for _, addr := range invalidCases {
+		assert.False(t, validator.IsAddressFormatValid(addr, Mainnet), addr)
+	}
