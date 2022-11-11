9a31f6f96f21a338d61ad40ee7cf459a5752ffb1
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-11-11 13:46:59 +0000

    add status

--- validator/tezos_test.go
+++ validator/tezos_test.go
@@ -2,3 +2,13 @@ package validator
 
 import (
 	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTezosValidateAddress(t *testing.T) {
+	validator := &Tezos{}
+
+	var validCases = map[string]*Result{
+		"tz1
