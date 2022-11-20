6bcd3fb6cefb7b8127ce04c6881aa2f797f16079
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-11-20 09:52:51 +0000

    remove grunt

--- validator/tezos_test.go
+++ validator/tezos_test.go
@@ -39,4 +39,6 @@ func TestTezosValidateAddress(t *testing.T) {
 
 	for addr, result := range invalidCases {
 		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mainnet), result), addr)
-		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, 
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Testnet), result), addr)
+	}
+}
---
+++ validator/tron.go
@@ -0,0 +1,6 @@
+package validator
+
+import "github.com/btcsuite/btcutil/base58"
+
+// Tron - Tron address validator
+type Tron stru
