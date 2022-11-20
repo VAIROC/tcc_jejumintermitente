77c4c4e540fdbc05a52883568e21b0470d83236b
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-11-20 21:56:01 +0000

    v0.9.12

--- validator/tron.go
+++ validator/tron.go
@@ -3,4 +3,9 @@ package validator
 import "github.com/btcsuite/btcutil/base58"
 
 // Tron - Tron address validator
-type Tron stru
+type Tron struct{}
+
+var _ Validator = (*Tron)(nil)
+
+// ValidateAddress - Check a Ripple address is valid or not
+func (r *Tron) ValidateAddress(addr string, network NetworkType) *Res
