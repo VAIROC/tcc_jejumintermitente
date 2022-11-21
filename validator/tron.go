d0b0d06f9813b00964219c9a6e0ac67bb41db010
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-11-21 14:15:41 +0000

    datepicker

--- validator/tron.go
+++ validator/tron.go
@@ -8,4 +8,7 @@ type Tron struct{}
 var _ Validator = (*Tron)(nil)
 
 // ValidateAddress - Check a Ripple address is valid or not
-func (r *Tron) ValidateAddress(addr string, network NetworkType) *Res
+func (r *Tron) ValidateAddress(addr string, network NetworkType) *Result {
+	decoded, version, err := base58.CheckDecode(addr)
+	if err != nil || len(decoded) != 20 || version != 65 {
+		return &Result{Success, false, Unknown,
