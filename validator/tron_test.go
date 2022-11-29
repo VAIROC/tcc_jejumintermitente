c7e681111877d5bc867e50b0cb2038db66de9f6c
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-11-29 05:54:30 +0000

    schema fix

--- validator/tron_test.go
+++ validator/tron_test.go
@@ -11,4 +11,6 @@ func TestTronValidateAddress(t *testing.T) {
 	validator := &Tron{}
 
 	var validCases = map[string]*Result{
-		"TKTcfBEKpp5ZRPwmiZ8SfLx8W7CDZ7PHCY": {Success, t
+		"TKTcfBEKpp5ZRPwmiZ8SfLx8W7CDZ7PHCY": {Success, true, Normal, ""},
+		"TW73UWp1a5s8Zs36D6LkQuBp27dz2VRg1a": {Success, true, Normal, ""},
+		"TLyqzVGLV1srkB7dToTAEqgDSfPtXRJZYH": {Success, tru
