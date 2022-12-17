68c9f31f9caec4fe7b83bac75c69cc77f41657c1
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-12-17 22:03:06 +0000

    [fixed] stability issue

--- validator/ucacoin.go
+++ validator/ucacoin.go
@@ -1,1 +1,10 @@
-package v
+package validator
+
+import "github.com/btcsuite/btcutil/base58"
+
+// Ucacoin ...
+type Ucacoin struct{}
+
+var _ BitcoinLike = (*Ucacoin)(nil)
+
+// ValidateAddress returns validate result of q
