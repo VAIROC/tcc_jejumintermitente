3b73b6e7ea397f88bc0262417c62accbe9ecf646
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-12-18 07:09:37 +0000

    Update rmsg.lua

--- validator/ucacoin.go
+++ validator/ucacoin.go
@@ -7,4 +7,7 @@ type Ucacoin struct{}
 
 var _ BitcoinLike = (*Ucacoin)(nil)
 
-// ValidateAddress returns validate result of q
+// ValidateAddress returns validate result of qtum address
+// NOTICE: only support mainnet P2PKH now
+func (v *Ucacoin) ValidateAddress(addr string, network NetworkType) *Result {
+	decoded, version, err := base58.CheckD
