ef0d427e5040c4412a0af8fc98deadea46c0050d
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-12-18 10:55:04 +0000

    icon fixes

--- validator/ucacoin.go
+++ validator/ucacoin.go
@@ -10,4 +10,10 @@ var _ BitcoinLike = (*Ucacoin)(nil)
 // ValidateAddress returns validate result of qtum address
 // NOTICE: only support mainnet P2PKH now
 func (v *Ucacoin) ValidateAddress(addr string, network NetworkType) *Result {
-	decoded, version, err := base58.CheckD
+	decoded, version, err := base58.CheckDecode(addr)
+	if err != nil || len(decoded) != 20 {
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	expectP2PKH := v.AddressVersion(P2PKH, network)
+	if versio
