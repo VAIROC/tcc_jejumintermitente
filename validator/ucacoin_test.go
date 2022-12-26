cd83c6e48c26ffba49ce6213c6365f8226ea6944
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-12-26 18:13:25 +0000

    bump version to 0.0.4

--- validator/ucacoin_test.go
+++ validator/ucacoin_test.go
@@ -4,4 +4,11 @@ import (
 	"reflect"
 	"testing"
 
-	"github.co
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUcacoinValidateAddress(t *testing.T) {
+	validator := &Ucacoin{}
+
+	var mainnetCases = map[string]*Result{
+		"UaFYCsCHXtbW4EW
