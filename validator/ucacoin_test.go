95dbeab4f77fcc923708553048da739bf865deb6
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-12-27 18:08:44 +0000

    Add installation instructions to README.md

--- validator/ucacoin_test.go
+++ validator/ucacoin_test.go
@@ -13,4 +13,9 @@ func TestUcacoinValidateAddress(t *testing.T) {
 	var mainnetCases = map[string]*Result{
 		"UaFYCsCHXtbW4EWmckYXP8BmGgezJrRdem": {Success, true, P2PKH, ""},
 		"Uak5JfpmLyjJkGJP37eBWHVPQmYo9wLX7w": {Success, true, P2PKH, ""},
-		"UbehDN7bgkJ8VgCsM96r2W2UpRoiHS
+		"UbehDN7bgkJ8VgCsM96r2W2UpRoiHSQizY": {Success, true, P2PKH, ""},
+		"UcAkUrswRtP6vwZQf2F26k68dyHqWG7Sw5": {Success, true, P2PKH, ""},
+	}
+
+	for addr, result := range mainnetCases {
+		assert.True(t, refl
