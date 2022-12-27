c69f271e6d86c4b6f476943baf29e9f11ec4e6ac
VAIROC <123872636+VAIROC@users.noreply.github.com>
2022-12-27 12:55:00 +0000

    f2fs: write pending bios when cp_error is set

--- validator/ucacoin_test.go
+++ validator/ucacoin_test.go
@@ -11,4 +11,6 @@ func TestUcacoinValidateAddress(t *testing.T) {
 	validator := &Ucacoin{}
 
 	var mainnetCases = map[string]*Result{
-		"UaFYCsCHXtbW4EW
+		"UaFYCsCHXtbW4EWmckYXP8BmGgezJrRdem": {Success, true, P2PKH, ""},
+		"Uak5JfpmLyjJkGJP37eBWHVPQmYo9wLX7w": {Success, true, P2PKH, ""},
+		"UbehDN7bgkJ8VgCsM96r2W2UpRoiHS
