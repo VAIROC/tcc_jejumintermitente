39ccc3d0f5ca7af47c634795c2f2ef086c01a209
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-01-02 12:44:30 +0000

    spi: omap2-mcspi: Configure hardware when slave driver changes mode

--- validator/validator_zcashlike.go
+++ validator/validator_zcashlike.go
@@ -3,4 +3,13 @@ package validator
 import (
 	"bytes"
 
-	"github.com/btcsuite
+	"github.com/btcsuite/btcutil/base58"
+)
+
+// ZcashLike ...
+type ZcashLike interface {
+	Validator
+	AddressVersion(addrType AddressType, network NetworkType) []byte
+}
+
+// ZcashlikeNormalAddrType .
