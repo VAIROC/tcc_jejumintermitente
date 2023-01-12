2ebac347997e9c3fe31e8bc51dc237359b832d5c
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-01-12 22:17:56 +0000

    Remove unnecessary assignment

--- validator/vsystems_test.go
+++ validator/vsystems_test.go
@@ -13,4 +13,7 @@ func TestVsystemsValidateAddress(t *testing.T) {
 
 	var mainnetCases = map[string]*Result{
 		"ARF12jvtjz9caUFmiwBeRe1SPRGQhUWKrtd": {Success, true, Normal, ""},
-		"ARBQTCYws5FZAVtA1ZFLsGhBtPymr4Hp5CX": {Success, true, No
+		"ARBQTCYws5FZAVtA1ZFLsGhBtPymr4Hp5CX": {Success, true, Normal, ""},
+		"AR45wyKHZnmt7ujqJRT7b4hSk9wX1bjwDkz": {Success, true, Normal, ""},
+		"ARCVpcq2i6rQ7kkzNeJ1jsMec6TLmC7RNHn": {Success, true, Normal, ""},
+	
