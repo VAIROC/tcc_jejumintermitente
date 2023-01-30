6dda5aa039f01a7ec8055c4b29ff6b55b94230ed
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-01-30 22:49:23 +0000

    refact

--- validator/wayfcoin.go
+++ validator/wayfcoin.go
@@ -10,3 +10,25 @@ var _ BitcoinLike = (*Wayfcoin)(nil)
 
 // ValidateAddress returns validate result of qtum address
 // NOTICE: only support mainnet P2PKH now
+func (v *Wayfcoin) ValidateAddress(addr string, network NetworkType) *Result {
+	decoded, version, err := base58.CheckDecode(addr)
+	if err != nil || len(decoded) != 20 {
+		return &Result{Success, false, Unknown, ""}
+	}
+
+	expectP2PKH := v.AddressVersion(P2PKH, network)
+	if version == expectP2PKH {
+		return &Result{Success, true, P2PKH, ""}
+	}
+
+	return &Result{Success, false, Unknown, ""}
+}
+
+// AddressVersion returns qtum address version according to the address type and
+// network type
+func (v *Wayfcoin) AddressVersion(addrType AddressType, network NetworkType) byte {
+	if addrType == P2PKH && network == Mainnet {
+		return 73
+	}
+	panic(ErrUnsupported.Error())
+}
