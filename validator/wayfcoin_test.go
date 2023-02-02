a21f9de52452254849629f2af47f800945e6ada8
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-02-02 19:50:19 +0000

    test passing

--- validator/wayfcoin_test.go
+++ validator/wayfcoin_test.go
@@ -7,4 +7,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWayfcoinVali
+func TestWayfcoinValidateAddress(t *testing.T) {
+	validator := &Wayfcoin{}
+
+	var mainnetCases = map[string]*Result{
+		"WaaKv3fUm2s3H4qwpDTVdHNwjhxPoLSQFH": {Success, true, P2PKH, ""},
+		"WcPL
