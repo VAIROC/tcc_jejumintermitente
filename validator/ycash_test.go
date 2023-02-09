b6127515be43c2a726998c396e8563e1f9e53da5
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-02-09 08:36:52 +0000

    Fix email address.

--- validator/ycash_test.go
+++ validator/ycash_test.go
@@ -8,4 +8,9 @@ import (
 )
 
 // TODO: testnet cases
-func TestYcashValidateAddress(t *testing.T
+func TestYcashValidateAddress(t *testing.T) {
+	validator := &Ycash{}
+
+	var mainnetCases = map[string]*Result{
+		"s1jF4pKgr8izDWNLZ9D1AtJfWFhgCY1Qexd": {Success, true, P2PKH, ""},
+		"s1MLh26NK6cMAPQqxBbuJcKVgo1C1tBkT3j": {Success, true
