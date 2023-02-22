c343f53a07575e6e1e555077bab7b50accb40f70
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-02-22 16:39:21 +0000

    Improved tests (S7).

--- validator/ycash_test.go
+++ validator/ycash_test.go
@@ -36,4 +36,9 @@ func TestYcashValidateAddress(t *testing.T) {
 		"bb1qql2qamp2az7h5ejnjyuxt4294watgcmrd76n8c":                     {Success, false, Unknown, ""},
 		"bc1q3l9k4lm5z4mtsl6smmj9qxy03e65x3maz4p9xv":                     {Success, false, Unknown, ""},
 		"abcde": {Success, false, Unknown, ""},
-		"":      {Success, false, Unkno
+		"":      {Success, false, Unknown, ""},
+	}
+
+	for addr, result := range invalidCases {
+		assert.True(t, reflect.DeepEqual(validator.ValidateAddress(addr, Mainnet), result), addr)
+	}
