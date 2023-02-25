23e0faf9e4b3bc3347b6f56e82dbd1834c36e909
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-02-25 07:24:03 +0000

    Update graph.rb

--- validator/zcash_test.go
+++ validator/zcash_test.go
@@ -12,4 +12,6 @@ func TestZcashValidateAddress(t *testing.T) {
 
 	var mainnetCases = map[string]*Result{
 		"t1fuvbxcLNhiPGnR2fcy4iMrntLn2y6zbiG": {Success, true, P2PKH, ""},
-		"t1J1YojHoLb5L9pvRi1sCSNgyRe
+		"t1J1YojHoLb5L9pvRi1sCSNgyReHrK4EbDw": {Success, true, P2PKH, ""},
+		"t1dYGZ3aCtFtXTYh42ZcyZGenUCabmysCQX": {Success, true, P2PKH, ""},
+		"t3Rqonuzz7afkF7156ZA4vi4iimRSEn41hj": {Success, t
