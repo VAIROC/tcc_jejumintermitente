5b7f1a1284390ce573554de8264e3e5d036fde3c
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-03-16 18:23:32 +0000

    new model

--- validator/zeepin_test.go
+++ validator/zeepin_test.go
@@ -4,4 +4,11 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/stret
+	"github.com/stretchr/testify/assert"
+)
+
+func TestZeepinValidateAddress(t *testing.T) {
+	validator := &Zeepin{}
+
+	var mainnetCases = map[string]*Result{
+		"ZQfyK6J8sRtKT5XE
