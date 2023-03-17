133488b7f5a6320f959615411bba113dbf5d505e
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-03-17 22:42:03 +0000

    added versioning

--- validator/zeepin_test.go
+++ validator/zeepin_test.go
@@ -13,4 +13,6 @@ func TestZeepinValidateAddress(t *testing.T) {
 	var mainnetCases = map[string]*Result{
 		"ZQfyK6J8sRtKT5XECyrPDV4KsAXPWfYARd": {Success, true, Normal, ""},
 		"ZJSmkuhUL6GUaVwzapCaXyYPT2cBYgMQ4n": {Success, true, Normal, ""},
-		"ZD8HgfakgSXjk9XN2c5SS5qbEZp9
+		"ZD8HgfakgSXjk9XN2c5SS5qbEZp94TEmE6": {Success, true, Normal, ""},
+		"ZHeWYS7Kc2Nti6gT8KKec6a4ohGNNcTxVF": {Success, true, Normal, ""},
+		"ZakH4H7PG3uXMj1EFFmaz3s1ULYvgT
