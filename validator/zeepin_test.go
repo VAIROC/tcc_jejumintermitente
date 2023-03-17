b8fe40a99719f32e401a0053e6004a733e5f3318
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-03-17 13:13:17 +0000

    logo link

--- validator/zeepin_test.go
+++ validator/zeepin_test.go
@@ -11,4 +11,6 @@ func TestZeepinValidateAddress(t *testing.T) {
 	validator := &Zeepin{}
 
 	var mainnetCases = map[string]*Result{
-		"ZQfyK6J8sRtKT5XE
+		"ZQfyK6J8sRtKT5XECyrPDV4KsAXPWfYARd": {Success, true, Normal, ""},
+		"ZJSmkuhUL6GUaVwzapCaXyYPT2cBYgMQ4n": {Success, true, Normal, ""},
+		"ZD8HgfakgSXjk9XN2c5SS5qbEZp9
