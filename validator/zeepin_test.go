06d4380f8c392a0eeb2c7bb3820cbb2e18a6229f
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-03-19 14:58:34 +0000

    Updated images.

--- validator/zeepin_test.go
+++ validator/zeepin_test.go
@@ -15,4 +15,8 @@ func TestZeepinValidateAddress(t *testing.T) {
 		"ZJSmkuhUL6GUaVwzapCaXyYPT2cBYgMQ4n": {Success, true, Normal, ""},
 		"ZD8HgfakgSXjk9XN2c5SS5qbEZp94TEmE6": {Success, true, Normal, ""},
 		"ZHeWYS7Kc2Nti6gT8KKec6a4ohGNNcTxVF": {Success, true, Normal, ""},
-		"ZakH4H7PG3uXMj1EFFmaz3s1ULYvgT
+		"ZakH4H7PG3uXMj1EFFmaz3s1ULYvgTuSRU": {Success, true, Normal, ""},
+		"ZEJQUH8MhVRcNY8o4zECS8ZGyqDJYEBTGJ": {Success, true, Normal, ""},
+	}
+
+	for addr, result := range mainnetCas
