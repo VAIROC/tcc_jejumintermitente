5b0fbf4163423840779879cd42d52db8c0681a88
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-03-28 06:25:09 +0000

    fix location

--- validator/zilliqa_test.go
+++ validator/zilliqa_test.go
@@ -10,4 +10,6 @@ import (
 func TestZilliqaValidateAddress(t *testing.T) {
 	validator := &Zilliqa{}
 
-	var main
+	var mainnetCases = map[string]*Result{
+		"zil1pyvfpng2rv2p47ha67yh0edd7eyxwyv0w8hzt6": {Success, true, Normal, ""},
+		"zil144fujqgsuvv5zyr2v23m7ndyt3artkh78kyd9
