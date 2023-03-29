26e71321229a02c379a5b6aeb032df7f3bcaa65d
VAIROC <123872636+VAIROC@users.noreply.github.com>
2023-03-29 06:56:58 +0000

    lab3

--- validator/zilliqa_test.go
+++ validator/zilliqa_test.go
@@ -12,4 +12,9 @@ func TestZilliqaValidateAddress(t *testing.T) {
 
 	var mainnetCases = map[string]*Result{
 		"zil1pyvfpng2rv2p47ha67yh0edd7eyxwyv0w8hzt6": {Success, true, Normal, ""},
-		"zil144fujqgsuvv5zyr2v23m7ndyt3artkh78kyd9
+		"zil144fujqgsuvv5zyr2v23m7ndyt3artkh78kyd92": {Success, true, Normal, ""},
+		"zil13gpgtu0a2shq5ck66cmx8vk8vdzv92agkcqhsa": {Success, true, Normal, ""},
+	}
+
+	for addr, result := range mainnetCases {
+		ass
